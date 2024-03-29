a0501c15e00aa7c66b521c5e46d8a1c6fe232664
Chatter Chats <chats@irby.us>
2024-03-28 20:08:28 -0400

    Cleanup data imports by using an imports.py file

--- diveharder/main.py
+++ diveharder/main.py
@@ -14,20 +14,7 @@ from fastapi.middleware.cors import CORSMiddleware
 import diveharder.data.api as api
 
 # DATA MODEL IMPORTS
-from diveharder.models.arrowhead.warStatus import WarStatus
-from diveharder.models.arrowhead.warInfo import WarInfo
-from diveharder.models.arrowhead.warSummary import WarSummary
-from diveharder.models.arrowhead.warId import WarID
-from diveharder.models.arrowhead.newsFeed import NewsFeedItem
-from diveharder.models.arrowhead.assignment import Assignment
-from diveharder.models.arrowhead.timeSinceStart import TimeSinceStart
-from diveharder.models.arrowhead.levelspec import Level
-from diveharder.models.arrowhead.leaderboard import Leaderboard, Error
-from diveharder.models.arrowhead.newsticker import NewsTicker
-from diveharder.models.arrowhead.galacticWarEffects import GalacticWarEffect
-from diveharder.models.arrowhead.missionrewards import MissionRewards
-from diveharder.models.arrowhead.item import Item
-from diveharder.models.arrowhead.all import AllRaw
+from diveharder.models.arrowhead.arrowhead_imports import *
 
 description = """
 Python FastAPI proxy server for the Helldivers 2 API.
