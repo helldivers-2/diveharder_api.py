9f49b424146aaf7849f185f3c3f2e7552b17d5b2
Chatter Chats <chats@irby.us>
2024-04-05 16:36:06 -0400

    reformat of settings layout to handle new security tokens, and simply some of the code in files the relied on settings.py

--- diveharder/cfg/settings.py
+++ diveharder/cfg/settings.py
@@ -7,33 +7,42 @@ if os.path.isfile("./.env"):
 news_feed_max_entries = "?maxEntries=1024"
 steam_news_query_params = "?appid=553850&feeds=steam_community_announcements"
 session_token: str = str(os.environ["SESSION_TOKEN"])
-auth = os.environ["API_TOKEN"]
 
-api = {
-    "BASE_URL": os.environ["BASE_URL"],
-    "TIME_DELAY": os.environ["TIME_DELAY"],
-    "TIMEOUT": os.environ["TIMEOUT"],
-    "STATUS_API_URL": os.environ["STATUS_API_URL"],
-    "WARINFO_API_URL": os.environ["WARINFO_API_URL"],
-    "NEWS_TICKER_API_URL": os.environ["NEWS_TICKER_API_URL"],
-    "NEWS_FEED_API_URL": os.environ["NEWS_FEED_API_URL"] + news_feed_max_entries,
-    "TIMESINCESTART_API_URL": os.environ["TIMESINCESTART_API_URL"],
-    "WARID_API_URL": os.environ["WARID_API_URL"],
-    "GALACTICE_WAR_EFFECTS_API_URL": os.environ["GALACTICE_WAR_EFFECTS_API_URL"],
-    "LEVELSPEC_API_URL": os.environ["LEVELSPEC_API_URL"],
-    "ITEMS_API_URL": os.environ["ITEMS_API_URL"],
-    "MISSION_REWARD_API_URL": os.environ["MISSION_REWARD_API_URL"],
-    "HOTF_LEADERBOARD_API_URL": os.environ["HOTF_LEADERBOARD_API_URL"],
-    "PLANET_STATS_API_URL": os.environ["PLANET_STATS_API_URL"],
-    "MAJOR_ORDER_API_URL": os.environ["MAJOR_ORDER_API_URL"],
-    "STOREFRONT_API_URL": os.environ["STOREFRONT_API_URL"],
-    "STOREFRONT_ROTATION_API_URL": os.environ["STOREFRONT_ROTATION_API_URL"],
-    "SEASONPASS_API_URL": os.environ["SEASONPASS_API_URL"],
-    "SCORE_CALC_PARAMS_API_URL": os.environ["SCORE_CALC_PARAMS_API_URL"],
-    "MINIGAME_LEADERBOARD_API_URL": os.environ["MINIGAME_LEADERBOARD_API_URL"],
-    "HOTF_CLAN_LEADBOARD_API_URL": os.environ["HOTF_CLAN_LEADBOARD_API_URL"],
-    "ELECTION_CANDIDATES_API_URL": os.environ["ELECTION_CANDIDATES_API_URL"],
-    "ELECTION_TERMS_API_URL": os.environ["ELECTION_TERMS_API_URL"],
-    "ELECTION_POLICIES_API_URL": os.environ["ELECTION_POLICIES_API_URL"],
-    "STEAM_NEWS_API_URL": os.environ["STEAM_NEWS_API_URL"] + steam_news_query_params,
+security = {
+    "key": os.environ["SECURITY_KEY"],
+    "algorithm": os.environ["SECURITY_ALG"],
+    "token": os.environ["SECURITY_TOKEN"],
+}
+
+BASE_URL = os.environ["BASE_URL"]
+TIME_DELAY = os.environ["TIME_DELAY"]
+TIMEOUT = os.environ["TIMEOUT"]
+
+urls = {
+    "auth": [
+        BASE_URL + os.environ["STATUS_API_URL"],
+        BASE_URL + os.environ["WARINFO_API_URL"],
+        BASE_URL + os.environ["PLANET_STATS_API_URL"],
+        BASE_URL + os.environ["MAJOR_ORDER_API_URL"],
+        BASE_URL + os.environ["NEWS_FEED_API_URL"] + news_feed_max_entries,
+        BASE_URL + os.environ["WARID_API_URL"],
+        BASE_URL + os.environ["TIMESINCESTART_API_URL"],
+        BASE_URL + os.environ["NEWS_TICKER_API_URL"],
+        BASE_URL + os.environ["GALACTICE_WAR_EFFECTS_API_URL"],
+        BASE_URL + os.environ["LEVELSPEC_API_URL"],
+        BASE_URL + os.environ["ITEMS_API_URL"],
+        BASE_URL + os.environ["MISSION_REWARD_API_URL"],
+        BASE_URL + os.environ["STOREFRONT_ROTATION_API_URL"],
+        BASE_URL + os.environ["STOREFRONT_API_URL"],
+        BASE_URL + os.environ["SEASONPASS_API_URL"],
+        BASE_URL + os.environ["SCORE_CALC_PARAMS_API_URL"],
+        BASE_URL + os.environ["MINIGAME_LEADERBOARD_API_URL"],
+        BASE_URL + os.environ["ELECTION_CANDIDATES_API_URL"],
+        BASE_URL + os.environ["ELECTION_TERMS_API_URL"],
+        BASE_URL + os.environ["ELECTION_POLICIES_API_URL"],
+    ],
+    "unauth": [
+        BASE_URL + os.environ["HOTF_LEADERBOARD_API_URL"],
+        os.environ["STEAM_NEWS_API_URL"] + steam_news_query_params,
+    ],
 }
