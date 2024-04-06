06e5932d56a0cf75581359d87c6e36c87b179f50
issamoxix <issamhaidaoui@gmail.com>
2024-04-06 14:46:24 +0200

    Refactor file loading and downloading logic

--- diveharder/cfg/constants.py
+++ diveharder/cfg/constants.py
@@ -1,4 +1,6 @@
 import json
+import os
+import requests
 from .settings import session_token
 
 api = {
@@ -12,6 +14,23 @@ api = {
         "Authorization": session_token,
     },
 }
+
+files = ["planets.json", "sectors.json", "factions.json"]
+path = "./json/"
+repo_url = os.environ.get("FILES_URL")
+
+def load_json(file: str):
+    file_url = repo_url + file
+    response = requests.get(file_url, timeout=5)
+    response.raise_for_status()
+
+    os.makedirs(path, exist_ok=True)
+
+    with open(path + file, "w") as f:
+        f.write(response.text)
+
+
+check_files = [load_json(file) for file in files if not os.path.exists(path + file)]
 
 planets = {
     "names": json.load(open("./json/planets.json")),
