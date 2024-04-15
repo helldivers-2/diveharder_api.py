f29f6384df56248f34d9d81be38c4190897ba209
Chats <chats@irby.us>
2024-04-15 11:02:19 -0400

    Implement all /raw/* routes

--- src/routes/raw.py
+++ src/routes/raw.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 from src.data.api import handler
 
 api = handler.api
@@ -16,4 +16,14 @@ async def get_all_raw(source: str = ""):
     for i, (k, v) in enumerate(api.raw_data.items()):
         if v["data"] is not [] or {}:
             data[k] = v["data"]
+    return data
+
+
+@router.get("/raw/{id}", status_code=200)
+async def get_raw(id: str):
+    """Return api data where id is the key value found in `/raw/all`\n
+    Example: `/raw/status` would return the status from `/raw/all`"""
+    update = await api.update_all()
+    info = api.raw_data.get(id, {"error": status.HTTP_400_BAD_REQUEST})
+    data = info.get("data")
     return data
