666b3380026a1d95d6ff6cbaabbb61a0565211e7
Chats <chats@irby.us>
2024-04-15 09:31:30 -0400

    Add Token Length Check

--- src/utils/authentication.py
+++ src/utils/authentication.py
@@ -6,8 +6,16 @@ TOKEN = security["token"]
 hashed = bcrypt.hashpw(bytes(TOKEN, "utf-8"), bcrypt.gensalt())
 
 
-def verify_password(plain_password):
-    if bcrypt.checkpw(str.encode(plain_password), hashed):
-        return True
-    else:
+def verify_length(token):
+    length = len(token)
+    if length <= 7 or length >= 25:
         return False
+    return True
+
+
+def verify_password(plaintext_token):
+    if verify_length(plaintext_token):
+        if bcrypt.checkpw(str.encode(plaintext_token), hashed):
+            return True
+        else:
+            return False
