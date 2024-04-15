1fb83f843524eb962a2ee8899c7665a0c44eb59f
Chats <chats@irby.us>
2024-04-15 11:42:49 -0400

    Add Github link, update Discord

--- src/main.py
+++ src/main.py
@@ -10,7 +10,8 @@ app = FastAPI(
     description="""
     Proxy API for Helldivers 2, providing access to backend game systems
     used by Helldivers 2. Built on the Python FastAPI framework.
-    Contact: chatterchats@discord""",
+    Discord: chatterchats
+    Github: https://github.com/helldivers-2/diveharder_api.py/""",
 )
 
 # noinspection PyTypeChecker
