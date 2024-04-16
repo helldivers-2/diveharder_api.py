52df736ac603a8721ed0217495d0e15cd7af7ee4
Chats <chats@irby.us>
2024-04-16 13:52:59 -0400

    implement middleware for case insensitive routes

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, APIRouter
+from fastapi.requests import Request
 from starlette.middleware.cors import CORSMiddleware
 
 
@@ -24,6 +25,19 @@ app.add_middleware(
 )
 
 router = APIRouter()
+
+
+@app.middleware("http")
+async def case_sens_middleware(request: Request, call_next):
+    decode_format = "utf-8"
+    raw_query_str = request.scope["query_string"].decode(decode_format).lower()
+    request.scope["query_string"] = raw_query_str.encode(decode_format)
+
+    path = request.scope["path"].lower()
+    request.scope["path"] = path
+
+    response = await call_next(request)
+    return response
 
 
 app.include_router(router)
