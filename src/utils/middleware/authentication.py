6992565adb3c9baf7cf1fc08ba15691ffcca88ca
Chats <chats@irby.us>
2024-04-30 12:48:14 -0400

    implement authentication middleware as middleware

--- src/utils/middleware/authentication.py
+++ src/utils/middleware/authentication.py
@@ -1,9 +1,33 @@
 import bcrypt
 
+from fastapi import status
+from fastapi.requests import Request
+from fastapi.responses import JSONResponse
+
 from src.cfg.settings import security
 
 TOKEN = security["token"]
 hashed = bcrypt.hashpw(bytes(TOKEN, "utf-8"), bcrypt.gensalt())
+
+
+async def authenticate(request: Request, call_next):
+    authenticated_prefixes = "admin"
+    response = None
+    headers = dict(request.scope["headers"])
+    if (
+        authenticated_prefixes in request.url.path
+        and "authorization" in request.headers
+    ):
+        if verify_password(request.headers["Authorization"]):
+            response = await call_next(request)
+        else:
+            response = JSONResponse(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                content={"error": "Unauthorized"},
+            )
+    else:
+        response = await call_next(request)
+    return response
 
 
 def verify_length(token):
