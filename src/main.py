5118b7c85e528bcc32c2d9a329db79c101d034ce
Chats <chats@irby.us>
2024-04-30 09:03:59 -0400

    Properly implement new file layout.

--- src/main.py
+++ src/main.py
@@ -1,9 +1,43 @@
 from fastapi import FastAPI
-from src.utils.api_singleton import fast_app as fast_app
+from starlette.middleware.base import BaseHTTPMiddleware
+
+# Middleware Improts
+from starlette.middleware.cors import CORSMiddleware
+from brotli_asgi import BrotliMiddleware
+from src.utils.middleware.case_sens_middleware import case_sens_middleware
+from src.utils.middleware.rate_limit import cutoff
+from src.utils.middleware.metrics import instrumentator
+
+# Routes Import
 from src.routes import base, admin, raw, v1
 
-app: FastAPI = fast_app
+# -------- INIT API -------- #
+app: FastAPI = FastAPI(
+    title="HD2 Community API",
+    openapi_url="/openapi.json",
+    description="""
+    Proxy API for Helldivers 2, providing access to backend game systems
+    used by Helldivers 2. Built on the Python FastAPI framework.
+    Discord: chatterchats
+    Github: https://github.com/helldivers-2/diveharder_api.py/""",
+)
 
+# -------- MIDDLEWARE -------- #
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.add_middleware(BrotliMiddleware)
+app.add_middleware(BaseHTTPMiddleware, dispatch=case_sens_middleware)
+app.add_middleware(BaseHTTPMiddleware, dispatch=cutoff)
+instrumentator.instrument(app=app).expose(
+    app, include_in_schema=False, should_gzip=True
+)
+
+# -------- ROUTES -------- #
 app.include_router(base.router)
 app.include_router(admin.router)
 app.include_router(raw.router)
