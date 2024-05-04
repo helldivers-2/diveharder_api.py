040d8aba8c74ff49f48d2c0db4e6c0f6fbd00fb0
Chats <chats@irby.us>
2024-05-03 20:49:50 -0400

    refactor rate_limit function

--- src/utils/middleware/rate_limit.py
+++ src/utils/middleware/rate_limit.py
@@ -1,22 +1,14 @@
-import src.utils.log as log
 from fastapi.requests import Request
-from fastapi.responses import JSONResponse
+from fastapi.responses import PlainTextResponse as PTR
 import re
 
 
-async def cutoff(request: Request, call_next):
-    regex_pattern = r"Helldivers%20Companion.*CFNetwork.*Darwin"
+async def rate_limit(request: Request, call_next):
+    regex_patterns = [r"Helldivers%20Companion.*", r"Heckdiver.*"]
+    client_id = request.headers.get("User-Agent", request.headers.get("X-Super-Client"))
 
-    decode_format = "utf-8"
-    raw_query_str = request.headers.get("User-Agent", "")
-    if re.search(regex_pattern, raw_query_str):
-        response = JSONResponse(
-            status_code=429,
-            content={
-                "limited": "Contact @chatterchats on Discord.",
-            },
-        )
-        return response
-    else:
-        response = await call_next(request)
-        return response
+    for pattern in regex_patterns:
+        if re.search(pattern, client_id):
+            return PTR(status_code=429, content="", headers={"content-type": ""})
+
+    return await call_next(request)
