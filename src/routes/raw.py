733b4fd57eba0a11508b1f5d9068b9526a8933c6
Chats <chats@irby.us>
2024-05-03 20:48:25 -0400

    improve error handling in webhook

--- src/routes/raw.py
+++ src/routes/raw.py
@@ -7,12 +7,15 @@ import src.utils.log as log
 
 api = handler.api
 
-router = APIRouter(tags=["raw"])
+router = APIRouter(
+    prefix="/raw",
+    tags=["raw"],
+    responses={404: {"description": "Not found"}},
+)
 
 
-@router.get("/all", include_in_schema=False)
-@router.get("/raw/all", status_code=200)
-async def get_all_raw(request: Request, source: str = ""):
+@router.get("/all", status_code=200)
+async def get_all_raw(request: Request):
     """
     All AHGS API Calls Only\n
     Note: Updates from Steam News are parsed from BBCode to Markdown.\n
@@ -21,27 +24,147 @@ async def get_all_raw(request: Request, source: str = ""):
       the proper authentication we have to rely on rednecked solutions.\n
     """
     await api.update_all()
+    no_include = [
+        "items",
+        "store_rotation",
+        "mission_rewards",
+        "level_spec",
+        "score_calc",
+        "season_pass",
+        "season_pass_hm",
+        "season_pass_sv",
+        "season_pass_ce",
+        "season_pass_dd",
+        "minigame_leaderboard",
+        "player_leaderboard",
+        "commend_leaderboard",
+        "election_policies",
+        "election_candidates",
+    ]
     data = {}
-    for i, (k, v) in enumerate(api.raw_data.items()):
-        if v["data"] is not [] or {}:
+    for k, v in api.raw_data.items():
+        if v["data"] and k not in no_include:
             data[k] = v["data"]
-    log.info(request, status.HTTP_200_OK, source)
+    log.info(request, status.HTTP_200_OK)
     return data
 
 
-@router.get("/raw/planetstats", include_in_schema=False)
-async def redirect_planetstats():
-    return RedirectResponse("/raw/planet_stats")
-
-
-@router.get("/raw/{data_id}", status_code=200)
-async def get_raw(request: Request, data_id: str, source: str = ""):
-    """Return api data where id is the key value found in `/raw/all`\n
-    Example: `/raw/status` would return the status from `/raw/all`\n
-    Note: Updates from Steam News are parsed from BBCode to Markdown."""
-    log.info(request, status.HTTP_200_OK, source)
-    if data_id in api.raw_data.keys():
-        await api.fetch_data(info_name=data_id)
-        data = api.raw_data[data_id].get("data")
+@router.get("/status", status_code=200)
+async def redirect_planetstats(request: Request):
+    log.info(request, status.HTTP_200_OK)
+    await api.fetch_data(info_name="status")
+    data = api.raw_data["status"].get("data")
+    if data:
         return data
     raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+
+
+@router.get("/war_info", status_code=200)
+async def redirect_planetstats(request: Request):
+    log.info(request, status.HTTP_200_OK)
+    await api.fetch_data(info_name="war_info")
+    data = api.raw_data["war_info"].get("data")
+    if data:
+        return data
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+
+
+@router.get("/planetstats", include_in_schema=False)
+@router.get("/planet_stats")
+async def redirect_planetstats(request: Request):
+    log.info(request, status.HTTP_200_OK)
+    await api.fetch_data(info_name="planet_stats")
+    data = api.raw_data["planet_stats"].get("data")
+    if data:
+        return data
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+
+
+@router.get("/major_order", status_code=200)
+async def redirect_planetstats(request: Request):
+    log.info(request, status.HTTP_200_OK)
+    await api.fetch_data(info_name="major_order")
+    data = api.raw_data["major_order"].get("data")
+    if data:
+        return data
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+
+
+@router.get("/news_feed", status_code=200)
+async def redirect_planetstats(request: Request):
+    log.info(request, status.HTTP_200_OK)
+    await api.fetch_data(info_name="news_feed")
+    data = api.raw_data["news_feed"].get("data")
+    if data:
+        return data
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+
+
+@router.get("/updates", status_code=200)
+async def redirect_planetstats(request: Request):
+    log.info(request, status.HTTP_200_OK)
+    await api.fetch_data(info_name="updates")
+    data = api.raw_data["updates"].get("data")
+    if data:
+        return data
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+
+
+@router.get("/war_id", status_code=200)
+async def redirect_planetstats(request: Request):
+    log.info(request, status.HTTP_200_OK)
+    await api.fetch_data(info_name="war_id")
+    data = api.raw_data["war_id"].get("data")
+    if data:
+        return data
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+
+
+@router.get("/items", status_code=200)
+async def redirect_planetstats(request: Request):
+    log.info(request, status.HTTP_200_OK)
+    await api.fetch_data(info_name="war_id")
+    data = api.raw_data["war_id"].get("data")
+    if data:
+        return data
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+
+
+@router.get("/minigame_leaderboard", status_code=200)
+async def redirect_planetstats(request: Request):
+    log.info(request, status.HTTP_200_OK)
+    await api.fetch_data(info_name="minigame_leaderboard")
+    data = api.raw_data["minigame_leaderboard"].get("data")
+    if data:
+        return data
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+
+
+@router.get("/player_leaderboard", status_code=200)
+async def redirect_planetstats(request: Request):
+    log.info(request, status.HTTP_200_OK)
+    await api.fetch_data(info_name="player_leaderboard")
+    data = api.raw_data["player_leaderboard"].get("data")
+    if data:
+        return data
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+
+
+@router.get("/commend_leaderboard", status_code=200)
+async def redirect_planetstats(request: Request):
+    log.info(request, status.HTTP_200_OK)
+    await api.fetch_data(info_name="commend_leaderboard")
+    data = api.raw_data["commend_leaderboard"].get("data")
+    if data:
+        return data
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+
+
+@router.get("/clan_leaderboard", status_code=200)
+async def redirect_planetstats(request: Request):
+    log.info(request, status.HTTP_200_OK)
+    await api.fetch_data(info_name="clan_leaderboard")
+    data = api.raw_data["clan_leaderboard"].get("data")
+    if not data:
+        return {}
+    return data
