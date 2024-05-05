bf5eb6cea898c38964158ff75a9bf63634d9353d
Chats <chats@irby.us>
2024-05-05 17:57:51 -0400

    Cleanup Rate Limit middleware

--- src/utils/middleware/rate_limit.py
+++ src/utils/middleware/rate_limit.py
@@ -4,11 +4,13 @@ import re
 
 
 async def rate_limit(request: Request, call_next):
-    regex_patterns = [r"Helldivers%20Companion.*", r"Heckdiver.*"]
-    client_id = request.headers.get("User-Agent", request.headers.get("X-Super-Client"))
+    regex_patterns = [r"Helldivers%20Companion.*", r"Heckdivers.*"]
+    headers = request.headers
+    client_id = headers.get("User-Agent", headers.get("X-Super-Client", ""))
 
     for pattern in regex_patterns:
         if re.search(pattern, client_id):
             return PTR(status_code=429, content="", headers={"content-type": ""})
+            # return None, return "", return - All Error Out
 
     return await call_next(request)
